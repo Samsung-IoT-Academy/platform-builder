2ee4140b058f6af3fb517476d667286482ae8b89
Georgiy Odisharia <math.kraut.cat@gmail.com>
2017-09-08 20:50:14 +0300

    Init commit
    Signed-off-by: Georgiy Odisharia <math.kraut.cat@gmail.com>

---
+++ lib/perl/KernelBuilder.pm
@@ -0,0 +1,67 @@
+package KernelBuilder;
+
+use strict;
+use warnings;
+use v5.10;
+
+use Scalar::Util "openhandle";
+
+use Exporter "import";
+our @EXPORT = qw(merge_kconfig);
+
+sub merge_kconfig {
+    my ($main_kconfig_path, $partial_kconfig_path) = @_;
+
+    open(my $main_fh, '+<', $main_kconfig_path) || die "fff";
+    open(my $partial_fh, '<', $partial_kconfig_path) || die "die";
+
+    my %config = hashify_conf($partial_fh);
+    my @main_content = <$main_fh>;
+    @main_content = map {
+        if (m/^(?:#\ )?(?<conf_name>[\w_]*)[\s=](?<status>[ynm]|is\ not\ set)$/ and
+            $config{$+{conf_name}} and $config{$+{conf_name}} =~ /[ynm]/) {
+            my $status = $config{$+{conf_name}};
+            say $status;
+            delete $config{$+{conf_name}};
+            $+{conf_name} . "=" . $status
+        } else {
+            $_
+        }
+    } @main_content;
+    foreach my $conf_opt (keys %config) {
+        push @main_content, $conf_opt . "=" . $config{$conf_opt}
+    }
+    push @main_content, "\n";
+    print $main_fh @main_content;
+
+    close($main_fh);
+    close($partial_fh);
+}
+
+sub hashify_conf {
+    my $file = shift;
+    my $fh = openhandle($file);
+    my $opened_in_sub = undef;
+    my %hsh = ();
+
+    if (!$fh) {
+        $opened_in_sub = 1;
+        open($fh, '<', $file);
+        if (!openhandle($file)) {
+            exit
+        }
+    }
+
+    while (<$fh>) {
+        m/^(?:#\ )?(?<conf_name>[\w_]*)[\s=](?<status>[ynm]|is\ not\ set)$/;
+        $hsh{$+{conf_name}} = $+{status}
+    }
+
+    if ($opened_in_sub) {
+        close($fh)
+    }
+
+    return %hsh;
+}
+
+1;
